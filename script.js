545276b830e5a030b68ab6fb5430d7e8f7044caf
Milos Adnadjevic <milos@Miloss-Air.lan>
2022-12-17 17:06:31 -0500

    ajax working

---
+++ script.js
@@ -0,0 +1,36 @@
+const $firstName = $('#first_name');
+const $lastName = $('#last_name');
+const $position = $('#position');
+const $team = $('#team')
+const $input = $('input[type="text"]')
+
+let playerData, userInput;
+
+$("form").on("submit", handleGetData)
+
+function handleGetData(event) {
+  event.preventDefault()
+  
+  userInput = $input.val()
+  console.log(userInput)
+  $.ajax({
+    url: "https://www.balldontlie.io/api/v1/players?search=" + userInput,
+  }).then(
+    (data) => {
+      playerData = data.data[0]
+      render()
+    },
+    (error) => {
+      console.log("bad request", error)
+    }
+  )
+}
+
+function render() {
+    $firstName.text(playerData.first_name);
+    $lastName.text(playerData.last_name);
+    $position.text(playerData.position);
+    $team.text(playerData.team.full_name);
+}
+
+$team.style.color = 'red'
